fde7a6d570a92eef64ced06f5e5cc99b4878ecf2
thanmaisai-asc <thanmai.sai@ascendion.com>
2024-08-21 12:40:30 +0530

    buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View,Image } from "react-native";
+import React, {Component} from 'react';
+import {Alert, Button, StyleSheet, View} from 'react-native';
+
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this._onPressButton} title="Press Me" />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button onPress={this._onPressButton} title="This looks great!" />
+          <Button onPress={this._onPressButton} title="OK!" color="#841584" />
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20,
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 });
-
-export default function Index() {
-  return (
-    <>
-      <Image source={require("../assets/images/react-logo.png")} />
-
-    </>
-  );
-}
