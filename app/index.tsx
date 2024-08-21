eece65950ba4ad235c3d07f868dff9ac1d939b6d
thanmaisai-asc <thanmai.sai@ascendion.com>
2024-08-21 11:36:35 +0530

    inline - Percentage Dimensions

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,50 @@
 import { StyleSheet, Text, View } from "react-native";
 
-
 const styles = StyleSheet.create({
   container: {
     marginTop: 50,
   },
   bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
+    color: "blue",
+    fontWeight: "bold",
     fontSize: 30,
   },
   red: {
-    color: 'red',
+    color: "red",
   },
 });
 
 export default function Index() {
   return (
-  <>
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={styles.bigBlue}>Edit app/index.tsx to edit this screen.</Text>
-      
-    </View>
-    <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-      <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-      <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+    <>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={styles.bigBlue}>
+          Edit app/index.tsx to edit this screen.
+        </Text>
+      </View>
+      <View style={{ flex: 1, height: "15%", backgroundColor: "powderblue" }} />
+      <View
+        style={{
+          flex: 2,
+          width: "66%",
+          height: "35%",
+          backgroundColor: "skyblue",
+        }}
+      />
+      <View
+        style={{
+          flex: 3,
+          width: "33%",
+          height: "50%",
+          backgroundColor: "steelblue",
+        }}
+      />
     </>
   );
 }
