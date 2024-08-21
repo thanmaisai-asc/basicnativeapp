3aae46c4eee1477e2d4288a11086f5dc0acbf909
thanmaisai-asc <thanmai.sai@ascendion.com>
2024-08-21 11:23:19 +0530

    Flex Dimensions in inline styling

--- app/index.tsx
+++ app/index.tsx
@@ -17,6 +17,7 @@ const styles = StyleSheet.create({
 
 export default function Index() {
   return (
+  <>
     <View
       style={{
         flex: 1,
@@ -25,13 +26,11 @@ export default function Index() {
       }}
     >
       <Text style={styles.bigBlue}>Edit app/index.tsx to edit this screen.</Text>
-      <View
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: 'skyblue',
-        }}
-      />
+      
     </View>
+    <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+      <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+      <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+    </>
   );
 }
